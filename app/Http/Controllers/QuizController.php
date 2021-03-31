d85a0989f72fe66ddaa5c43fe7dfa56dd68bb8c7
Steve Lefevre <lefevre.10@osu.edu>
2021-03-31 14:20:53 -0400

    Fleshed out quiz controller

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class QuizController extends Controller
 {
-    //
+    public static function quiz() {
+        return view('quiz', [
+            'questions' => Questions::all(),
+        ]); 
+    }
 }
