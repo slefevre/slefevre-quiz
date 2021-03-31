21a1a4e81bbe4ec4bd6dfae8d743ed7ae3af1304
Steve Lefevre <lefevre.10@osu.edu>
2021-03-31 14:00:12 -0400

    Added questions to questions seeder

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -13,6 +13,33 @@ class QuestionSeeder extends Seeder
      */
     public function run()
     {
-        
+    	$i = 0;
+        DB::table('questions')->insert(
+            [
+                'wording' => "What is the average air speed velocity of a laden swallow?",
+                'created_at' => Carbon::now(),
+                'sort' => ++$i,
+            ],
+            [
+                'wording' => "What do you get when you multiply six by nine?",
+                'created_at' => Carbon::now(),
+                'sort' => ++$i,
+            ],
+            [
+                'wording' => "What is your father's middle name?",
+                'created_at' => Carbon::now(),
+                'sort' => ++$i,
+            ],
+            [
+                'wording' => "How many licks does it take to get to the tootsie roll center of a tootsie pop?",
+                'created_at' => Carbon::now(),
+                'sort' => ++$i,
+            ],
+            [
+                'wording' => "What would you do for a Klondike bar?",
+                'created_at' => Carbon::now(),
+                'sort' => ++$i,
+            ],
+        );
     }
 }
